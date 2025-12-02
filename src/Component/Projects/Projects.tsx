1e7e50ee7227604550057fd127447ddb5589b104
zaynabmohamed <zm2438320@gmail.com>
2025-12-03 00:44:02 +0200

    updata

--- src/Component/Projects/Projects.tsx
+++ src/Component/Projects/Projects.tsx
@@ -180,7 +180,7 @@ export default function Projects() {
     {
       Desc: "Developed a full-featured E-commerce website using React.js, Next.js, and TypeScript, providing users with a smooth and dynamic shopping experience. Implemented Context API for state management, React Hook Form for handling user input and validation, and integrated APIs to manage products, carts, and wishlists",
       name: "E-Commerce",
-      designation: "{React.js,Next.js,context,Api, TypeScript, React-hook-form,Tailwindcss,Tools UI}",
+      designation: "React.js,Next.js,context,Api, TypeScript, React-hook-form,Tailwindcss,Tools UI",
       src: Ecommerce,
       GitHub: "https://github.com/zaynabmohamed/E-Commerce-Route.git",
       LiveDemo: "https://e-commerce-route-a5q7.vercel.app",
@@ -188,7 +188,7 @@ export default function Projects() {
     {
       Desc: "Note App using Next.js, Tailwind CSS, and Hero UI for a clean and modern interface. Integrated Axios for API communication and React Hook Form for efficient form handling. Added Toast notifications to enhance user experience and provide instant feedback. The app allows users to add, edit, and delete notes easily with a smooth and responsive design.",
       name: "Notes App",
-      designation: "{Next.js,Axios,Hero UI,Tailwindcss,toolsUi }",
+      designation: "Next.js,Axios,Hero UI,Tailwindcss,toolsUi",
       src: Notes,
       GitHub: "https://github.com/zaynabmohamed/Notes.Next.js.git",
       LiveDemo: "https://notes-next-js-fawn.vercel.app",
@@ -196,7 +196,7 @@ export default function Projects() {
     {
       Desc: "Developed a responsive multi-section website using HTML, CSS, Bootstrap, and Media Queries. The project focuses on clean design, smooth layout, and full responsiveness across all devices. Bootstrap was used to structure and style the layout efficiently, while custom CSS and media queries ensured a unique and adaptive user interface.",
       name: "Dansial",
-      designation: "{Html,Css,Bootstrip MediaQuery}",
+      designation: "Html,Css,Bootstrip MediaQuery",
       src: Dansial,
       GitHub: "https://github.com/zaynabmohamed/Assigement-6-DevFoilo.git",
       LiveDemo: "https://daniels-exam-route-fdgg.vercel.app",
@@ -204,15 +204,14 @@ export default function Projects() {
     {
       Desc: "using React, Tailwind CSS, and Hero UI, allowing users to create profiles, share posts, like, and comment.Integrated Axios and React Query for efficient API data fetching and synchronization,while React Hook Form was used for smooth and validated user input handling",
       name: "Social.App",
-      designation: "{React,Hero Ui,Tailwindcss,ReactQuery, React-hook-form,axios }",
+      designation: "React,Hero Ui,Tailwindcss,ReactQuery, React-hook-form,axios ",
       src: Social,
       GitHub: "https://github.com/zaynabmohamed/Social.App.git",
       LiveDemo: "https://social-app-1779.vercel.app",
     },
     {
-      Desc: "using React, Tailwind CSS, and Hero UI, allowing users to create profiles, share posts, like, and comment.Integrated Axios and React Query for efficient API data fetching and synchronization,while React Hook Form was used for smooth and validated user input handling",
-      name: "Real-Estate",
-      designation: "{Frontend=> Next.js and Tailwind , Backend=> Convex and Database  , Authentication=> clerk}",
+      Desc: "built with Next.js and Tailwind CSS, allowing users to explore properties, view detailed and filter homes based on their needs",
+      designation: "Frontend=> Next.js and Tailwind , Backend=> Convex and Database  , Authentication=> clerk",
       src: RealEstate,
       GitHub: "https://github.com/zaynabmohamed/Real_Estate.git",
       LiveDemo: "https://real-estate-bspv.vercel.app",
